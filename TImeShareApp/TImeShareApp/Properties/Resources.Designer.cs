a6cca1c2f02ab553d1a051dc2d780909c326c587
Sotig <sotiris.papaioannou@hotmail.com>
2016-09-17 02:14:38 +0300

    update

--- TImeShareApp/TImeShareApp/Properties/Resources.Designer.cs
+++ TImeShareApp/TImeShareApp/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace TImeShareApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap _01_progress {
-            get {
-                object obj = ResourceManager.GetObject("01-progress", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap ajax_loader {
             get {
                 object obj = ResourceManager.GetObject("ajax-loader", resourceCulture);
